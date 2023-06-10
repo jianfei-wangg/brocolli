01fbfc9851da95fe387a37711aef1eea1bea6072
inisis <desmond.yao@buaa.edu.cn>
2023-06-10 13:21:39 +0800

    update

--- brocolli/quantization/observer.py
+++ brocolli/quantization/observer.py
@@ -166,7 +166,7 @@ class MinMaxObserver(_ObserverBase):
             return x_orig
         x = x_orig.detach()  # avoid keeping autograd tape
         x = x.to(self.min_val.dtype)
-        min_val_cur, max_val_cur = torch._aminmax(x)
+        min_val_cur, max_val_cur = torch.aminmax(x)
         min_val = torch.min(min_val_cur, self.min_val)
         max_val = torch.max(max_val_cur, self.max_val)
         self.min_val.copy_(min_val)
