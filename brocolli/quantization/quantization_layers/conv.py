e455f6bfd1cc9f38849b1be695ad8367773c852e
inisis <desmond.yao@buaa.edu.cn>
2023-06-16 22:22:20 +0800

    update conv

--- brocolli/quantization/quantization_layers/conv.py
+++ brocolli/quantization/quantization_layers/conv.py
@@ -65,36 +65,12 @@ class _ConvNd(nn.Module):
                 )
             )
         self.padding_mode = padding_mode
-
-        if self.transposed:
-            weight_shape = [in_channels, out_channels // self.groups]
-        else:
-            weight_shape = [out_channels, in_channels // self.groups]
-        qweight = torch._empty_affine_quantized(
-            weight_shape + list(kernel_size),
-            scale=1,
-            zero_point=0,
-            dtype=torch.qint8,
-            **{k: v for k, v in factory_kwargs.items() if k != "dtype"},
-        )
-        bias_float = (
-            torch.zeros(
-                out_channels,
-                dtype=torch.float,
-                **{k: v for k, v in factory_kwargs.items() if k != "dtype"},
-            )
-            if bias
-            else None
-        )
-
-        self.weight = qweight
-        self.bias = bias_float
         self.scale = 1.0
 
     def extra_repr(self):
         s = (
             "{in_channels}, {out_channels}, kernel_size={kernel_size}"
-            ", stride={stride}, act_scale={act_scale}, output_scale={output_scale}"
+            ", stride={stride}"
         )
         if self.padding != (0,) * len(self.padding):
             s += ", padding={padding}"
@@ -122,34 +98,9 @@ class _ConvNd(nn.Module):
     def get_qconv(
         cls,
         mod,
-        activation_pre_process,
-        weight_post_process=None,
-        activation_post_process=None,
     ):
-        r"""Creates a qconv object and returns it."""
-        if weight_post_process is None:
-            weight_post_process = mod.qconfig.weight()
-
-        weight_post_process(mod.weight)
-        qweight, wt_scale = _quantize_weight(mod.weight.float(), weight_post_process)
-
-        act_scale = activation_pre_process.calculate_qparams()
-        logger.debug(
-            f"{mod.name} activation scale: {act_scale}, max_val: {activation_pre_process.max_val}, min_val: {activation_pre_process.min_val}"
-        )
-        output_scale = activation_post_process.calculate_qparams()
-        logger.debug(
-            f"{mod.name} output scale: {output_scale}, max_val: {activation_post_process.max_val}, min_val: {activation_post_process.min_val}"
-        )
-        qbias = (
-            _quantize_bias(mod.bias.float(), act_scale * wt_scale)
-            if mod.bias is not None
-            else None
-        )
-
-        assert (
-            weight_post_process.dtype == torch.qint8
-        ), "Weight observer must have a dtype of qint8"
+        weight_observer = mod.qconfig.weight()
+        weight_observer(mod.weight)
 
         qconv = cls(
             mod.in_channels,
@@ -163,44 +114,24 @@ class _ConvNd(nn.Module):
             mod.padding_mode,
         )
         qconv.name = mod.name
-        qconv.qbit = mod.activation_pre_process.qbit
-        qconv.float_weight = mod.weight
-        qconv.weight = torch.nn.Parameter(qweight, requires_grad=False)
+        qconv.weight_observer = weight_observer
+        qconv.weight = torch.nn.Parameter(mod.weight, requires_grad=False)
         if mod.bias is not None:
-            qconv.bias = torch.nn.Parameter(qbias, requires_grad=False)
-        qconv.act_scale = torch.nn.Parameter(act_scale, requires_grad=False).to(qweight.device)
-        qconv.wt_scale = torch.nn.Parameter(wt_scale.reshape(1, -1, 1, 1), requires_grad=False).to(qweight.device)
-        qconv.output_scale = torch.nn.Parameter(output_scale, requires_grad=False).to(qweight.device)
-        qconv.output_min_value = activation_post_process.min_val
-        qconv.output_max_value = activation_post_process.max_val
+            qconv.bias = torch.nn.Parameter(mod.bias, requires_grad=False)
 
         return qconv
 
     @staticmethod
     def from_float(cls, mod):
-        assert type(mod) == cls._FLOAT_MODULE, (
-            " brocolli."
-            + cls.__name__
-            + ".from_float only works for "
-            + cls._FLOAT_MODULE.__name__
-            + " but got:"
-            + str(type(mod))
-        )
         assert hasattr(
-            mod.activation_pre_process, "qconfig"
+            mod, "qconfig"
         ), "Conv float module must have qconfig defined."
-        activation_pre_process = mod.activation_pre_process
-        weight_post_process = activation_pre_process.qconfig.weight()
-        activation_post_process = mod.activation_post_process
-        return cls.get_qconv(
-            mod, activation_pre_process, weight_post_process, activation_post_process
-        )
+
+        return cls.get_qconv(mod)
 
 
 @register_quant_op(torch.nn.Conv2d)
 class Conv2d(_ConvNd, BaseOperator):
-    _FLOAT_MODULE = nn.Conv2d
-
     def __init__(
         self,
         in_channels,
@@ -236,26 +167,21 @@ class Conv2d(_ConvNd, BaseOperator):
         )
 
     def _get_name(self):
-        return "QuantizedConv2d"
+        return "TransformedConv2d"
 
     def forward(self, input):
-        if len(input.shape) != 4:
-            raise ValueError("Input shape must be `(N, C, H, W)`!")
-
         out = F.conv2d(
-            input.to(torch.float32),  # double may overflow for large input
-            self.weight.to(torch.float32),
-            self.bias.to(torch.float32),
+            input,  # double may overflow for large input
+            self.weight_observer(self.weight),
+            self.bias,
             self.stride,
             self.padding,
             self.dilation,
             self.groups,
-        )
-
-        out = out * self.act_scale * self.wt_scale / self.output_scale
-        out = self.clamp(out)
+        )        
 
         return out
+
 
     @classmethod
     def from_float(cls, mod):
